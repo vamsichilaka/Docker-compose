b68673528bf5716c2884149a23a2071e8a0c1a52
vamsichilaka <168081361+vamsichilaka@users.noreply.github.com>
2026-03-22 18:01:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class Handler(BaseHTTPRequestHandler):
         count = r.incr("visits")
 
         message = f"""
-        Hi Vamsi"Triggered via github actions"
+        Hi Vamsii"Triggered via github actions"
         Request Flow:
 
         Browser → Nginx → App → Redis
